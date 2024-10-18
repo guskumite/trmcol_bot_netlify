76ecc0b19cdc2f03acecf592e71ad9b54d1a069d
guskumite <gmolanoruiz2000@gmail.com>
2024-10-18 10:06:14 -0500

    Adicion de middleware sessions

--- functions/bot.js
+++ functions/bot.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 // Inicializa el bot con tu token (asegúrate de configurar la variable en Netlify)
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
+//Middleware para sesiones
+bot.use(session());
+
 // Middleware para enviar un mensaje antes de que el usuario use cualquier comando
 bot.use(async (ctx, next) => {
     if (!ctx.session || !ctx.session.started) {
